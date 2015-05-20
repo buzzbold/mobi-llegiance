3e5fe72b38e18b48e0f05a7ef6ec89a14348c37f
buzzbold <buzzbold@users.noreply.github.com>
2015-05-20 13:24:36 -0500

    Create patch.js

---
+++ patch.js
@@ -0,0 +1,23 @@
+$( document ).ready(function() {
+loadjscssfile("https://buzzbold.com/utility/kska/patch.css", "css");
+loadjscssfile("https://buzzbold.com/utility/kska/jquery.radiosforbuttons.min.js"
+, "js");
+ $('#ctl00_AllegMain_wrkEmail').attr('placeholder', 'me@mydomain.net');
+$('#ctl00_AllegMain_wrkPhone').attr('placeholder', '907-000-0000');
+});
+function loadjscssfile(filename, filetype){
+ if (filetype=="js"){ //if filename is a external JavaScript file
+        var fileref=document.createElement('script')
+        fileref.setAttribute("type","text/javascript")
+        fileref.setAttribute("src", filename)
+    }
+    else
+     if (filetype=="css"){ //if filename is an external CSS file
+        var fileref=document.createElement("link")
+        fileref.setAttribute("rel", "stylesheet")
+        fileref.setAttribute("type", "text/css")
+        fileref.setAttribute("href", filename)
+    }
+    if (typeof fileref!="undefined")
+        document.getElementsByTagName("head")[0].appendChild(fileref)
+}
